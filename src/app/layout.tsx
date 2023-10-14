4706485df50c67b2533625683eb7f7e989811797
Mikael <mikaelmuniz2001@gmail.com>
2023-10-14 00:41:04 -0300

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
+  icons: {
+    icon: '/img/logo-agdv.png'
+  },
   title: 'Jogo AgentesDev',
   description: 'O Jogo (você perdeu)',
 }
